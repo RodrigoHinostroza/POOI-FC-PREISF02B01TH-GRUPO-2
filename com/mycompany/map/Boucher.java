9dfb087dcd9f0b77cf47e9637d10a1d01c7ad2d9
RodrigoHinostroza <142445346+RodrigoHinostroza@users.noreply.github.com>
2023-11-15 14:27:19 -0500

    Add files via upload

---
+++ com/mycompany/map/Boucher.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.map;
+
+/**
+ *
+ * @author RODRIGO
+ */
+public class Boucher {
+    
+    private int id;
+    private String codigo;
+    private int cantidad;
+    
+
+    public Boucher() {
+    }
+
+    public Boucher(int id, String cod_pro, int cantidad) {
+        this.id = id;
+        this.codigo = codigo;
+        this.cantidad = cantidad;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    }
+    
+    
---
+++ com/mycompany/map/Productos.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.map;
+
+import java.util.Date;
+
+/**
+ *
+ * @author RODRIGO
+ */
+public class Productos {
+   
+    private String nombre;
+    private String codigo;
+    private int cantidad;
+    private Date fechaVencimiento;
+
+    // Constructor
+    public Productos(String nombre, String codigo, int cantidad, Date fechaVencimiento) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.cantidad = cantidad;
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    // Métodos de acceso
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    // Métodos de modificación
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+}
+
