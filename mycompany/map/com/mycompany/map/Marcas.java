34a25eeb7bb0dac19e752daf4c04f775c931466e
RodrigoHinostroza <142445346+RodrigoHinostroza@users.noreply.github.com>
2023-11-15 14:25:16 -0500

    Add files via upload

---
+++ mycompany/map/com/mycompany/map/Marcas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.map;
+
+/**
+ *
+ * @author RODRIGO
+ */
+public class Marcas {
+   
+    private String nombre;
+
+     public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Marcas(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
